a4c3741da1dd0fe39f24b73e30fb8ac20895e2d9
Xerenz <martingeo15@gmail.com>
2019-12-22 19:35:50 +0530

    got fetching from db alright

--- controller/event.controller.js
+++ controller/event.controller.js
@@ -1,6 +1,10 @@
 const FreeEvent = require("../models/freeEve.model");
 const PaidEvent = require("../models/paidEve.model");
+const Events = require("../models/event.model");
 
 exports.event_show = function(req, res) {
+    Events.find({}, function(err, events) {
+        console.log(events);
+    });
     res.render("event");
 };
--- insert_to_Workshop.js
+++ insert_to_Workshop.js
@@ -1,3 +1,5 @@
+const Workshop = require("./models/ws.model");
+
 
 // place your data here
 
@@ -33,20 +35,11 @@ db.once('open', function() {
     console.log("Connection Successful!");
     
     // define Schema
-    var WorkshopSchema = mongoose.Schema({
-      	name : {type: String, required: true},
-        label : {type: String},
-        branch : {type: String, required: true},
-        content: {type: String}
-    });
- 
-    // compile schema to model
-    var Workshops = mongoose.model('workshop', WorkshopSchema);
- 
+
     // documents array
 
     // save multiple documents to the collection referenced by Book Model
-    Workshops.collection.insert(data, function (err, docs) {
+    Workshop.collection.insert(data, function (err, docs) {
       if (err){ 
           return console.error(err);
       } else {
