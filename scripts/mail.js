c175712064d0bb88655979a45c2a830d0908a8f3
Xerenz <martingeo15@gmail.com>
2020-01-09 20:26:09 +0530

    mail ready akua

--- scripts/mail.js
+++ scripts/mail.js
@@ -140,4 +140,65 @@ function getEmails() {
 
 // mailer();
 
-sendMail();
+// sendMail();
+
+function sendMail() {
+    async.waterfall([
+        function(done) {
+            User.find({}, function(err, users) {
+                allUsers = [];
+                users.forEach(function(user) {
+                    allUsers.push({email : user.username, name : user.name});
+                });
+                done(err, allUsers);
+            });
+        },
+        function(allUsers, done) {
+            console.log(allUsers);
+            let smtpTransport = nodemailer.createTransport({
+                host : 'smtp.zoho.com',
+                auth : {
+                    user : 'hr@dhishna.org',
+                    pass : process.env.PASSWORD
+                }
+            });
+
+            allUsers.forEach(function(user) {
+                let msg = {
+                    to : user.email,
+                    from : 'hr@dhishna.org',
+                    subject : 'Register for Dhishna Workshops!',
+                    html : `Hey ${user.name}
+                    
+                    <p>It's great that you registered to Dhishna 2020, but if you haven't done so already register for our awesome workshops.
+                    Here are the links to them</p>
+
+                    <!--// ** TODO : Add a bit of discription for every workshop ** //-->
+                
+                    <a href="https://dhishna.org/workshop/5e08429c8ece7f639db3bd5f/knowmore">Cyber Warfare</a>
+                    <a href="https://dhishna.org/workshop/5e08429c8ece7f639db3bd5d/knowmore">Ducati</a>
+                    <a href="https://dhishna.org/workshop/5e08429c8ece7f639db3bd5c/knowmore">Humanoid Robot</a>
+                    <a href="https://dhishna.org/workshop/5e08429c8ece7f639db3bd5e/knowmore">Mecedese Benz overhaul</a>
+                    
+                    <p>Lookout for more exciting workshops to come at https://dhishna.org/workshop</p> 
+
+                    <!--// ** Psst.. All these workshops are IIT certified ;) are they? ** //-->
+
+                    <p>Looking forward to seeing you at one of our events.</p>
+
+                    <p>Regards
+                    Dhishna 2020</p>`
+                }
+
+                smtpTransport.sendMail(msg, function(err) {
+                    if (err) console.log(err);
+                    else console.log('mail sent to', email); 
+                    
+                    done('done');
+                });
+            });
+        }
+    ], function(err) {
+        return console.log(err);
+    });
+}
