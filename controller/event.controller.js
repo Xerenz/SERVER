4a63ff302ea2c88330197fc2eeafeb4c63dc3af2
Xerenz <martingeo15@gmail.com>
2020-02-02 20:04:05 +0530

    spotlight added

--- controller/event.controller.js
+++ controller/event.controller.js
@@ -1,5 +1,3 @@
-const FreeEvent = require("../models/freeEve.model");
-const PaidEvent = require("../models/paidEve.model");
 const Events = require("../models/event.model");
 
 
@@ -9,7 +7,7 @@ exports.event_show = function(req, res) {
     Events.find({}, function(err, events) {
         if (err) {
             console.log(err);
-            return res.render("home");
+            return res.redirect("/");
         }
         console.log(events);
         res.render("event", {events : events});
@@ -27,5 +25,28 @@ exports.label_show = function(req, res) {
         }
         console.log(events);
         res.render("workshop", {events : events});
+    });
+};
+
+
+exports.event_one_show = function(req, res) {
+    var id = req.params.id; 
+    console.log(id)
+    Events.findOne({_id:id}, function(err, event) {
+        console.log(event)
+        if (err) {
+            console.log(err);
+            return res.redirect("/event");
+        }
+                
+        if(req.user)
+        {
+            qstring = "?data_name="+req.user.name+"&data_email="+req.user.username+"&data_phone="+req.user.phone+"&data_readonly=data_name&data_readonly=data_email&data_readonly=data_phone";
+            res.render("event_details",{event : event,logStatus:true, q: qstring});
+        }
+        else
+        {
+            res.render("event_details",{event : event,logStatus:false});
+        }
     });
 };
