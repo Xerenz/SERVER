8b39084b92f568e0b7c7e9e01c2125c598f4418b
Xerenz <martingeo15@gmail.com>
2020-02-13 21:08:19 +0530

    dash complete

--- controller/manager.controller.js
+++ controller/manager.controller.js
@@ -1,5 +1,9 @@
 const User = require('../models/user.model');
 const Transaction = require("../models/transaction.model");
+const Event = require("../models/event.model");
+const Workshop = require("../models/ws.model");   
+
+const async = require("async");
 
 exports.data_show =  (req,res) => {
     Transaction.find({payment_for : req.params.event}, (err,docs) => {
@@ -19,3 +23,35 @@ exports.data_show =  (req,res) => {
 
     });
 }
+
+exports.branch_show = function(req, res) {
+    async.waterfall([
+        function(done) {
+            eventNames = [];
+            Event.find({branch : req.params.branch}, function(err, events) {
+                events.forEach(function(event) {
+                    eventNames.push(event.name);
+                });
+
+                done(err, eventNames);
+            });
+        },
+        function(eventNames, done) {
+            workshopNames = [];
+            Workshop.find({branch : req.params.branch}, function(err, workshops) {
+                workshops.forEach(function(ws) {
+                    workshopNames.push(ws.name);
+                });
+
+                let list = eventNames.concat(workshopNames);
+
+                done(err, list);
+            });
+        }
+    ], function(err, list) {
+        if (err) return console.log(err);
+
+        res.send(list);
+    });
+    
+};
--- routes/manager.route.js
+++ routes/manager.route.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const manager_controller = require("../controller/manager.controller");
 
 router.get("/:event/view", manager_controller.data_show);
+router.get("/dash/:branch", manager_controller.branch_show);
 
 module.exports = router;
