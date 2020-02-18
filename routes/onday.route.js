5477be8e00a6ebab6bafccfec697c816a7586a28
Jithinksatheesh <jithinksatheesh@gmail.com>
2020-02-18 10:58:32 +0530

    switch

--- routes/onday.route.js
+++ routes/onday.route.js
@@ -4,6 +4,7 @@ const onday_controller = require("../controller/onday.controller");
 
 router.get("/:branch/:event/view/", onday_controller.registration_show);
 router.get("/:branch/:event/new/", onday_controller.registration_spot_detail);
+router.get("/:branch/:event/status/", onday_controller.registration_status);
 router.get("/:branch/:event/view/update/:id/", onday_controller.registration_update_detail);
 router.get("/:branch/:event/view/attended/:id/", onday_controller.registration_attended_mark);
 router.get("/:branch/:event/view/winner/:rank/:id/", onday_controller.registration_winner_mark);
